605870bf24be88b303d77fa6ba53877682463f37
Rud3us <rodionpylnev@yandex.ru>
2025-09-06 12:46:58 +0300

    Test for 2 lab

---
+++ Laba_ISP-233_RodionPylnev/#2_laba/Program2.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Laba_ISP233_RodionPylnev
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Введите имя: ");
+            // string name = Console.ReadLine();
+            // Console.WriteLine("Введите возраст: ");
+            // int age = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Введите город: ");
+            // string city = Console.ReadLine();
+            
+            float iFloat = 4.5f;
+            decimal iDecimal = 4.5m;
+            var iDouble = 4.5;
+            Console.WriteLine($"{iFloat} \n{iDecimal} \n{iDouble}");;
+        }
+    }
+}
