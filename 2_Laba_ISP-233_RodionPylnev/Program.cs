17017babf24ac932a32491cf7fb36060e0bd5953
Rud3us <rodionpylnev@yandex.ru>
2025-09-08 14:06:31 +0300

    Update dir files

---
+++ 2_Laba_ISP-233_RodionPylnev/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Laba_ISP233_RodionPylnev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"ы");
+        }
+    }
+}
