29d2d29652b7ddbc4f681a8a4e5df6dbb55c726a
Rud3us <rodionpylnev@yandex.ru>
2025-09-10 16:18:45 +0300

    New 3 lab

---
+++ 3_Laba_ISP-233_RodionPylnev/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace MyNamespace;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        for (int i = 0; i <= 10; i++)
+        {
+            sum += i;
+            Console.WriteLine(sum);
+        }
+    }
+}
